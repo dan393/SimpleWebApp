e85a1bf35baf4cbcf6461327497b1eaf7085cafd
Dan Berbec <dan393@gmail.com>
2018-07-02 14:12:02 +0100

    Added Ivan and Dan to the dictionary.

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -6,6 +6,10 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("dan")) {
+            return "Dan is the greatest man on planet earth!!!";
+        } else if (query.toLowerCase().contains("ivan")) {
+            return "Ivan is the greatest man on planet earth!!!";
         }
         return "";
     }
